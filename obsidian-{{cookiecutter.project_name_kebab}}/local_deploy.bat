94d34f1e952130300b62b472a6f0bf75da8fc824
Yu Wang <yuwan@microsoft.com>
2022-02-24 21:40:36 -0800

    update local_deploy.bat, add mkdir

--- obsidian-{{cookiecutter.project_name_kebab}}/local_deploy.bat
+++ obsidian-{{cookiecutter.project_name_kebab}}/local_deploy.bat
@@ -1,3 +1,4 @@
+mkdir %1\.obsidian\plugins\obsidian-{{cookiecutter.project_name_kebab}}\
 copy main.js %1\.obsidian\plugins\obsidian-{{cookiecutter.project_name_kebab}}\
 copy manifest.json %1\.obsidian\plugins\obsidian-{{cookiecutter.project_name_kebab}}\
 @REM copy styles.css %1\plugins\obsidian-{{cookiecutter.project_name_kebab}}\
