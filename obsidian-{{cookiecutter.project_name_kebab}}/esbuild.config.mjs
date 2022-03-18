5b1b4933c4671369cc18b97324f74c8f47c0d2f3
Yu Wang <yuwan@microsoft.com>
2022-03-18 13:48:31 -0700

    feat: generate main file name by plugin name

--- obsidian-{{cookiecutter.project_name_kebab}}/esbuild.config.mjs
+++ obsidian-{{cookiecutter.project_name_kebab}}/esbuild.config.mjs
@@ -15,7 +15,7 @@ esbuild.build({
 	banner: {
 		js: banner,
 	},
-	entryPoints: ['main.ts'],
+	entryPoints: ['{{cookiecutter.project_acronym}}_main.ts'],
 	bundle: true,
 	external: [
 		'obsidian',
